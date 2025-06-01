29cde21fb11ddf88130cf69753c75e576e84b255
varunrnc <barunpandit143@gmail.com>
2025-06-01 15:23:19 +0530

    update

---
+++ time-complexity/program1.c
@@ -0,0 +1,24 @@
+// Fine the time complexity of the func1 function in the program show in program1.c as follows:
+#include<stdio.h>
+
+void func1(int array[], int length)
+{
+    int sum = 0;
+    int product = 1;
+    for(int i = 0; i < length; i++) 
+    {
+        sum += array[i];
+    }
+
+    for(int i=0; i < length; i++) 
+    {
+        product *= array[i];
+    }
+}
+
+int main() 
+{
+    int arr[] = {3, 5, 66};
+    func1(arr, 3);
+    return 0;
+}
